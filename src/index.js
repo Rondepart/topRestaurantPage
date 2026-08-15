228c329ea787a6872bd938307d2c8ff2c2807f9b
Mark <125941419+Rondepart@users.noreply.github.com>
2026-08-16 01:23:49 +0800

    Add btn event listener to show page if clicked

--- src/index.js
+++ src/index.js
@@ -6,6 +6,39 @@ import { createHomePageContent } from './homePage.js';
 import { createMenuPageContent } from './menu.js';
 import { createAboutPageContent } from './aboutPage.js';
 
-// createHomePageContent();
-// createMenuPageContent();
-createAboutPageContent();
+const nav = document.querySelector('.nav-btn-wrapper');
+const contentSection = document.querySelector('#content');
+
+createHomePageContent();
+
+//dont load page if page of the clicked button is already shown
+const webPage = [{page:'home'}, {page:'menu'}, {page:'about'}];
+let currentWebPage = webPage[0];
+
+nav.addEventListener('click', (event) => {
+    if (!event.target.matches('.nav-btn')) return;
+
+    if (event.target.matches('.home-btn')) {
+        if (currentWebPage.page === 'home') return;
+        
+        contentSection.innerHTML = '';
+        createHomePageContent();
+        currentWebPage = webPage[0];
+
+    } else if (event.target.matches('.menu-btn')) {
+        if (currentWebPage.page === 'menu') return;
+
+        contentSection.innerHTML = '';
+        createMenuPageContent();
+        currentWebPage = webPage[1];
+
+
+    } else if (event.target.matches('.about-btn')) {
+        if (currentWebPage.page === 'about') return;
+
+        contentSection.innerHTML = '';
+        createAboutPageContent();
+        currentWebPage = webPage[2];
+
+    }
+});
